789fc2410c191deca8779984bde4f732d243b508
xzp <3094997376@qq.com>
2022-08-08 14:10:47 +0800

    初始化

---
+++ Assets/Scripts/ProtoBufferHelpler.cs
@@ -0,0 +1,32 @@
+﻿    using System.Collections;
+using System.Collections.Generic;
+    using System.IO;
+    using System.Text;
+    using ProtoBuf;
+    using UnityEngine;
+
+public class ProtoBufferHelpler
+    {
+        //对象序列化成序列化成文件
+        public static string Serialize<T>(T t)
+        {
+            //MemoryStream继承了IDisposable接口，所以才能被using自动释放
+            //using 的作用为使用完之后就释放流对象
+            using (MemoryStream ms = new MemoryStream())
+            {
+                //使用protobuf程序集内置方法将对象t序列化到ms中
+                Serializer.Serialize<T>(ms, t);
+                return Encoding.UTF8.GetString(ms.ToArray());
+            }
+        }
+
+        //根据字符串反序列化成对象
+        public static T DeSerialize<T>(string content)
+        {
+            using (MemoryStream ms = new MemoryStream(Encoding.UTF8.GetBytes(content)))
+            {
+                T t = Serializer.Deserialize<T>(ms);
+                return t;
+            }
+        }
+    }  
---
+++ Assets/Scripts/TestPro.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.IO;
+using ProtoBuf;
+using Test.Person;
+
+
+public class TestPro : MonoBehaviour
+{
+    void Start()
+    {
+        //Person person1 = new Person() { Name = "小明同学", Age = 18 };
+
+        //序列化
+
+        //string testInfo = ProtoBufferHelpler.Serialize<Person>(person1);
+
+        //print("testInfo" + testInfo);
+
+        ////反序列化
+        //Person person2 = ProtoBufferHelpler.DeSerialize<Person>(testInfo);
+
+        //print("person2" + person2.Name + "  " + person2.Age);
+
+
+        person per=new person() { Name = "小明同学", Age = 19 }; ;
+        string testInfo = ProtoBufferHelpler.Serialize<person>(per);
+
+        print("testInfo" + testInfo);
+
+        //反序列化
+        person person2 = ProtoBufferHelpler.DeSerialize<person>(testInfo);
+
+        print("person  :" + person2.Name + "  " + person2.Age);
+
+    }
+}
+
+
+//定义一个序列化与反序列化对象，当然也可以是用protogen工具来实现
+
+[ProtoContract]
+class Person
+{
+    [ProtoMember(1)]
+    public string Name { get; set; }
+    [ProtoMember(2)]
+    public int Age { get; set; }
+}
